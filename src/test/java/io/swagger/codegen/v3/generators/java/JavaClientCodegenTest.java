3ff6ad1a2e88e0e86fbc1da9e5bc0fb43a8c6978
jpw <jan-peter.wilhelms@src-gmbh.de>
2019-02-25 17:01:26 +0100

    tests failed running on windows

--- src/test/java/io/swagger/codegen/v3/generators/java/JavaClientCodegenTest.java
+++ src/test/java/io/swagger/codegen/v3/generators/java/JavaClientCodegenTest.java
@@ -254,10 +254,10 @@ public class JavaClientCodegenTest {
     public void customTemplates() throws Exception {
         final JavaClientCodegen codegen = new JavaClientCodegen();
         codegen.processOpts();
-        Assert.assertEquals(codegen.templateDir(), "handlebars" + File.separator  + "Java");
+        Assert.assertEquals(codegen.templateDir(), "handlebars/Java");
 
-        codegen.additionalProperties().put(CodegenConstants.TEMPLATE_DIR, String.join(File.separator,"user", "custom", "location"));
+        codegen.additionalProperties().put(CodegenConstants.TEMPLATE_DIR, String.join("/", "user", "custom", "location"));
         codegen.processOpts();
-        Assert.assertEquals(codegen.templateDir(), String.join(File.separator,"user", "custom", "location"));
+        Assert.assertEquals(codegen.templateDir(), String.join("/", "user", "custom", "location"));
     }
 }
